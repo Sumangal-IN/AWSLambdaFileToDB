7ed64d61728de348243c475b09d4691a2322bba0
Sumangal-IN <mandas04@GBKTFDC013X0013.kits.kfplc.com>
2019-07-24 04:03:48 +0530

    New project

--- awslambda/src/main/java/com/home/awslambda/App.java
+++ awslambda/src/main/java/com/home/awslambda/App.java
@@ -3,10 +3,13 @@ package com.home.awslambda;
 import java.io.BufferedReader;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.Reader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.net.URLDecoder;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.Statement;
+
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVParser;
 
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
@@ -22,47 +25,35 @@ public class App implements RequestHandler<S3Event, String> {
 	public String handleRequest(S3Event s3event, Context context) {
 		try {
 			S3EventNotificationRecord record = s3event.getRecords().get(0);
-
 			String bucket = record.getS3().getBucket().getName();
-			String bucketKey = record.getS3().getObject().getKey()
-					.replace('+', ' ');
-
+			String bucketKey = record.getS3().getObject().getKey().replace('+', ' ');
 			bucketKey = URLDecoder.decode(bucketKey, "UTF-8");
-
 			AmazonS3 s3Client = AmazonS3ClientBuilder.standard().build();
-			S3Object s3Object = s3Client.getObject(new GetObjectRequest(bucket,
-					bucketKey));
+			S3Object s3Object = s3Client.getObject(new GetObjectRequest(bucket, bucketKey));
 
 			InputStream in = s3Object.getObjectContent();
-			BufferedReader br = new BufferedReader(new InputStreamReader(in));
-			String line = null;
-			Class.forName("com.mysql.jdbc.Driver");
-			Connection connection = null;
-			System.out.println("Trying to connect to DB ....");
-			connection = DriverManager
-					.getConnection("jdbc:mysql://testdb.cjotpija7r7c.us-east-1.rds.amazonaws.com:3306/dbname?"
-							+ "user=sumangal&password=linuxlinux");
-			if (connection != null) {
-				System.out.println("DB connection sucessful");
-				Statement stmt = connection.createStatement();
-				stmt.execute("create table stock_data (article_id varchar(10),store_id varchar(10),stock varchar(10))");
-				while ((line = br.readLine()) != null) {
-					System.out.println("insert into stock_data values ("
-							+ line.split(",")[0] + "," + line.split(",")[1]
-							+ "," + line.split(",")[2] + ")");
-					stmt.execute("insert into stock_data values ("
-							+ line.split(",")[0] + "," + line.split(",")[1]
-							+ "," + line.split(",")[2] + ")");
-				}
-				s3Object.close();
-			} else
-				System.out.println("DB connection failure");
-			connection.close();
+			Reader reader = new BufferedReader(new InputStreamReader(in));
 
-			System.out.println("DB connection closed ....");
+			CSVParser csvParser = new CSVParser(reader, CSVFormat.DEFAULT);
+			csvParser.getRecords().stream().forEach(r -> {
+				HttpURLConnection conn = null;
+				try {
+					conn = (HttpURLConnection) new URL(
+							String.format("https://load-app.herokuapp.com/records/%s/%s", r.get(0), r.get(1)))
+									.openConnection();
+				} catch (Exception e) {
+					e.printStackTrace();
+				} finally {
+					if (conn != null)
+						conn.disconnect();
+				}
+			});
+			csvParser.close();
+
+			return "Done";
 		} catch (Exception e) {
 			e.printStackTrace();
+			return "Failed";
 		}
-		return "Ok";
 	}
 }
