5ba25dc764b98f9308d4b6e3f13047c9fd9bf80a
Sumangal-IN <mandas04@GBKTFDC013X0013.kits.kfplc.com>
2019-07-24 05:15:27 +0530

    New project

--- awslambda/src/main/java/com/home/awslambda/App.java
+++ awslambda/src/main/java/com/home/awslambda/App.java
@@ -4,13 +4,11 @@ import java.io.BufferedReader;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.Reader;
-import java.net.HttpURLConnection;
-import java.net.URL;
 import java.net.URLDecoder;
 
 import org.apache.commons.csv.CSVFormat;
 import org.apache.commons.csv.CSVParser;
-
+import org.apache.http.client.fluent.Request;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.amazonaws.services.lambda.runtime.events.S3Event;
@@ -25,13 +23,9 @@ public class App implements RequestHandler<S3Event, String> {
 	public String handleRequest(S3Event s3event, Context context) {
 
 		try {
-			System.out.println(s3event.getRecords().size());
 			S3EventNotificationRecord record = s3event.getRecords().get(0);
-
 			String bucket = record.getS3().getBucket().getName();
-			System.out.println("bucket" + bucket);
 			String bucketKey = record.getS3().getObject().getKey().replace('+', ' ');
-			System.out.println("key" + record.getS3().getObject().getKey());
 			bucketKey = URLDecoder.decode(bucketKey, "UTF-8");
 			AmazonS3 s3Client = AmazonS3ClientBuilder.standard().build();
 			S3Object s3Object = s3Client.getObject(new GetObjectRequest(bucket, bucketKey));
@@ -41,17 +35,11 @@ public class App implements RequestHandler<S3Event, String> {
 
 			CSVParser csvParser = new CSVParser(reader, CSVFormat.DEFAULT);
 			csvParser.getRecords().stream().forEach(r -> {
-				HttpURLConnection conn = null;
 				try {
-					System.out.println(String.format("https://load-app.herokuapp.com/records/%s/%s", r.get(0), r.get(1)));
-					conn = (HttpURLConnection) new URL(
-							String.format("https://load-app.herokuapp.com/records/%s/%s", r.get(0), r.get(1)))
-									.openConnection();
+					Request.Post(String.format("https://load-app.herokuapp.com/records/%s/%s", r.get(0), r.get(1)))
+							.execute();
 				} catch (Exception e) {
 					e.printStackTrace();
-				} finally {
-					if (conn != null)
-						conn.disconnect();
 				}
 			});
 			csvParser.close();
